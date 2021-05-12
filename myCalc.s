391fa36aa4c3ef73985d73d38b58ecd351f103dd
liadatz <liad.atsmon@gmail.com>
2021-05-12 22:53:36 +0300

    12.5 22:53

--- myCalc.s
+++ myCalc.s
@@ -1,10 +1,10 @@
 
 
-section .data                                              ; we define (global) initialized variables in .data section
+section .data                                               ; we define (global) initialized variables in .data section
     counter_stack: dd 5                                     ; 4bytes stack counter- counts the number of free spaces
     counter: dd 0                                           ; 4bytes counter- counts the number of operations.
-    op_stack: TIMES 5 dd 0                                  ; initalize array of pointers in the size of 5 (default)
-    binary_value: TIMES 3 db 0                              ; 12 zero bits that will be modified to represent the binary value of an 4 chars operand
+    op_stack: dd 0                                          ; initalize an empty pointer
+    binary_value: dd 0                                      ; 16 zero bits that will be modified to represent the binary value of an 4 chars operand
 
 section	.rodata					                        ; we define (global) read-only variables in .rodata section
 	format_string: db "%s", 10, 0	                        ; format string for printf func
@@ -37,21 +37,27 @@ section .text
 main:
 
     init:
-        cmp byte [esp], 1                                     ; check if argc is greater the 1
+        cmp byte [esp], 1                                   ; check if argc is greater the 1
         jg modify_stack                                     ; we need to change the stack size
-        
-        mov ecx, op_stack                                   ; set ecx to point the top of the op_stack
+        push 5                                              ; argument for malloc func
+        call malloc
+        add esp, 4                                          ; clean stack after call
+        mov [op_stack], eax                                      
+
+        mov ecx, [op_stack]                                   ; set ecx to point the top of the op_stack
         jmp start_loop
 
     modify_stack:
         mov eax, [esp + 8]
-        mul eax, 4
+        mov ebx, 4
+        mul ebx
         push eax
         call malloc
         add esp, 4			                                ; clean up stack after call
-        mov op_stack, eax
-        mov counter_stack, [esp + 8]                        ; set number of free spaces in stack to argv[1]    
-        mov ecx, op_stack                                   ; set ecx to point the top of the op_stack
+        mov [op_stack], eax
+        mov eax, [esp + 8]
+        mov [counter_stack], eax                            ; set number of free spaces in stack to argv[1]    
+        mov ecx, [op_stack]                                 ; set ecx to point the top of the op_stack
         jmp start_loop
     
 
@@ -76,138 +82,147 @@ main:
         cmp byte [buffer], 57                               ; check if the input is lesser than '9'
         jle case_operand                                    ; if so, the input first char is a number, and we want to deal with the it as a operand
         
-      jmp case_operator                                   ; if not, then the input is an operator
+        jmp case_operator                                   ; if not, then the input is an operator
 
 
     case_operand:
-        mov edx, 0                                          
-        mov ebx, 1
+        mov edx, 0                                          ; current location in the String (char index)                                        
+        mov ebx, 1                                          ; first link flag
   
 
         operand_loop:
-            cmp byte [buffer+edx], 10
-            jz start_loop
-            jmp binary_convertor
+            cmp byte [buffer+edx], 10                       ; check if current char in '\n' (indicate no char left to read)
+            jz start_loop                                   ; if so, go back to the start of the loop
+            jmp binary_convertor                            ; otherwise, convert current string to binary 
 
         operand_loop_continue:
-            add edx, 4
-            cmp ebx, 1
-            je add_first_link
-            jmp add_link
+            add edx, 4                                      ; add 4 to the char index of the string
+            cmp ebx, 1                                      ; check if first link flag is on
+            je add_first_link                               ; if so, jump to add first link
+            jmp add_link                                    ; otherwise, jump to add link
         
 
 
         add_first_link:
-            cmp counter_stack, 0                             ; check for availible free space in stack
-            je stack_overflow                                   ; if stack is full prompt error message
+            cmp dword [counter_stack], 0                     ; check for availible free space in stack
+            je stack_overflow                                ; if stack is full prompt error message
 
-            push 7                                              ; push size of link in bytes
+            push 7                                           ; push size of link in bytes
             call malloc
-            add esp, 4			                                ; clean up stack after call
-            mov [ecx], eax                                      ; set current free space to new allocated space
-            mov byte [eax], binary_value                      ; set first byte in link
+            add esp, 4			                             ; clean up stack after call
+            push edx
+            mov [ecx], eax                                   ; set current free space to new allocated space
+            mov byte dl, [binary_value] 
+            mov byte [eax], dl                     ; set first byte in link
             inc eax
-            mov byte [eax], binary_value+1                  ; set second byte in link
+            mov byte dl, [binary_value+1] 
+            mov byte [eax], dl                  ; set second byte in link
             inc eax
-            mov byte [eax], binary_value+2                  ; set third byte in link
+            mov byte dl, [binary_value+2] 
+            mov byte [eax], dl                   ; set third byte in link
             inc eax
-            mov [eax], 0                                 ; set next link to be NULL
+            mov dword [eax], 0                                     ; set next link to be NULL
 
-            add ecx, 4                                          ; move ecx to the next availible free space
-            sub counter_stack, 1                                ; substract 1 free space from number of availible free space in stack
-            mov ebx, 0
+            add ecx, 4                                       ; move ecx to the next availible free space
+            sub dword [counter_stack], 1                     ; substract 1 free space from number of availible free space in stack
+            mov ebx, 0                                       ; set first link flag to 0
+            and dword [binary_value], 0                            ; reset binary value   
+            pop edx
 
-            and binary_value, 0
-        
-            jmp operand_loop
+            jmp operand_loop                                 ; jump to operand loop
 
             stack_overflow:
-                push overflow_string			                ; call printf with 2 arguments -  
-                push format_string			                    ; pointer to prompt message and pointer to format string
+                push overflow_string			             ; call printf with 2 arguments -  
+                push format_string			                 ; pointer to prompt message and pointer to format string
                 call printf
-                add esp, 8			                            ; clean up stack after call
+                add esp, 8			                         ; clean up stack after call
                 jmp start_loop
             
-
         add_link:
-            push 7                                              ; push size of link in bytes
+            push 7                                           ; push size of link in bytes
             call malloc
-            add esp, 4			                                ; clean up stack after call
-            mov ebx, [ecx-4]                                    ; temp
-            mov [ecx-4], eax                                    ; set current free space to new allocated space
-            mov byte [eax], binary_value                        ; set first byte in link
+            add esp, 4			                             ; clean up stack after call
+            push edx
+            mov ebx, [ecx-4]                                 ; save previous link
+            mov [ecx-4], eax                                 ; set current free space to new allocated space
+            mov dl, [binary_value]
+            mov byte [eax], dl                               ; set first byte in link
             inc eax
-            mov byte [eax], binary_value+1                  ; set second byte in link
+            mov dl, [binary_value+1]
+            mov byte [eax], dl                                 ; set second byte in link
             inc eax
-            mov byte [eax], binary_value+2                  ; set third byte in link
+            mov dl, [binary_value+2]
+            mov byte [eax], dl                   ; set third byte in link
             inc eax
-            mov [eax], ebx                                 ; set next link to be to be the previous link in [ecx]
+            mov [eax], ebx                                   ; set next link to be to be the previous link in [ecx]
         
-            and binary_value, 0
-
+            and dword [binary_value], 0                              ; reset binary value  
+            mov ebx, 0                                       ; set first link flag to 0
+            pop edx
             jmp operand_loop
 
 
         
         binary_convertor:
-            push ebp                                                                               
-            mov ebp, esp
-            pushad
+            push ebp                                         ; save stack pointer                                                       
+            mov ebp, esp                                     ; set stack to curr location
+            pushad                                           ; backup registers
             
-            mov ecx, buffer
-            add ecx, edx
-            mov eax, 2                                                              
-            mov edx, 1
-            jmp looping
+            mov ebx, buffer                                  ; set ecx <- pointer to start point of the input string
+            add ebx, edx                                     ; move ecx to point the first out of the next 4 chars
+            mov eax, 2                                       ; set eax <- offset to the number of the bit being calculated out of the 3 per each char         
+            mov edx, 1                                       ; set eax <- countes number of char that was converted so far
 
-            loopin:
-                mov ebx, 0
-                sub byte [ecx], 48                                                  ;get numeric value of the char
-                jmp dec_to_binary
+            looping:
+                mov ecx, 0                                   ; set ebx <-  num of bits written so far for one char (zero out of three)
+                sub byte [ebx], 48                           ; get numeric value of the char (according to ascii table)
 
             dec_to_binary:
-                cmp ebx, 3
-                jz next_char
-                shr byte [ecx], 1
-                jc with_carry
-                jmp without_carry
+                cmp ecx, 3                                   ; check if all three bits for the current char were written
+                jz next_char                                 ; if so, go to convert next char
+                shr byte [ebx], 1                            ; divide numeric value of first char by 2 
+                jc with_carry                                ; checks if after division carry flag is on
+                jmp without_carry                            ; check is after division carry flag is of
 
             with_carry:
-                or [binary_value+eax], 1                                            ; change 'zero' bit to 'one' bit
-                dec eax                                                             ; decrease number of bits counter
-                inc ebx
-                jmp dec_to_binary                                                 
+                or byte [binary_value+eax], 128              ; change 'zero' bit to 'one' bit ??? byte ??? or with 10000000
+                dec eax                                      ; decrease number of bits offset   
+                inc ecx                                      ; increase number of bits that were written
+                jmp dec_to_binary                            ; continue convert the same char 
 
             without_carry:
-                dec eax                                                             ; decrease number of bits counter
-                inc ebx
-                jmp dec_to_binary
+                dec eax                                      ; decrease number of bits counter
+                inc ecx                                      ; increase number of bits that were written
+                jmp dec_to_binary                            ; continue convert the same char
 
             next_char:
-                add eax, 6
-                inc ecx
-                inc edx
-                cmp byte [ecx], 10
-                jz finish_1
-                cmp edx, 5
-                jz finish_2
-                jmp looping
+                add eax, 5                                   ; add 5 to the offset to the bit that needs to be written
+                inc ebx                                      ; move the pointer to the next char out of the 4           
+                inc edx                                      ; increase the number of chars that were converted
+                cmp byte [ebx], 10                           ; checks if the current char that need to be converted is '\n'
+                jz finish_1                                  ; if so jump to first part of finish converting
+                cmp edx, 5                                   ; checks if 4 chars were already converted
+                jz finish_2                                  ; if so jumps to second part of finish converting
+                jmp looping                                  ; continue to convert the current char need to be converted
 
             finish_1:
-                mul ebx, 3
-                shr [binary_value], ebx
-                jmp finish_2
+                mov eax, ecx
+                mov ecx, 3
+                mul ecx                                     ; calculate the number of shifts need to be done to move the whole bits to the LSB
+                mov ecx, eax
+                shr dword [binary_value], cl                     ; shift the bits to the LSB side of the 'binary value' bits
+                jmp finish_2                                ; jump to second part of finish converting
 
             finish_2:
-                popad
-                mov esp, ebp
-                pop edp
-                jmp operand_loop_continue
+                popad                                       ; pop al register that was backuped
+                mov esp, ebp                                 
+                pop ebp
+                jmp operand_loop_continue                   ; continue to deal with the operand after value was converted
 
 
     case_operator:
         cmp byte [buffer], 113 	                            ; check if the input is 'q'
-	    jz quit_loop				                        ; if so quit the loop
+	    jz case_quit				                        ; if so quit the loop
 
         cmp byte [buffer], 43 	                            ; check if the input is '+'
 	    jz case_addition			
